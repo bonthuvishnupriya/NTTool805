7f017c67b433bf96d6da8d5e3d0a1f35f1cfd4ee
admin <admin@VenuMadhav>
2025-12-09 21:52:08 +0530

    Dev1 modification

--- GitProj01/src/main/java/com/nt/main/AppTest.java
+++ GitProj01/src/main/java/com/nt/main/AppTest.java
@@ -3,6 +3,7 @@ package com.nt.main;
 import com.nt.services.ArithmeticOperations;
 
 public class AppTest {	
+	
 	public static void main(String[] args){
 		ArithmeticOperations ops=new ArithmeticOperations();
 		System.out.println("Result (Sum)is: :"+ops.sum(10, 20));
